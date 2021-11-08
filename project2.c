2a329ee87099ebd4ea0974013fd71efd1fc275b1
Yunsir <93338661+monkeylovemusic@users.noreply.github.com>
2021-11-08 19:44:01 +0800

    Add files via upload

---
+++ project2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	printf("ÇëÊäÈënum1£¬num2:\n");
+	scanf("%d%d",&num1,&num2);
+	sum = num1 + num2;
+	printf("ºÍÎª:%d\n",sum);
+
+
+}
